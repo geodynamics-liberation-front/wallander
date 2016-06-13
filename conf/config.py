03458800693086045ecbcbab1597bf52f77b4e6a
Robert Petersen <rpetersen@ucsd.edu>
2016-06-13 12:30:03 -0500

    Added to flag to wallander module to prevent logging to a file, changed the logging instantation so that child loggers don't get a handler thereby removing duplicate log entries, removed extranious add_logger call in the data_providers module.

--- conf/config.py
+++ conf/config.py
@@ -10,12 +10,18 @@ LOG=logging.getLogger(__name__)
 LOG.setLevel(logging.WARN)
 
 # The name of the loggers to configure
-LOGS=[__name__+".LOG","__main__.LOG","wallander.LOG","wallander.application.LOG","StagYY.LOG","wallander.cache.LOG","wallander.viz.LOG",
-"wallander.data_providers.LOG","wallander.contour.LOG"]
+LOGS=[
+{'module':__name__,'log':'LOG','level':logging.DEBUG,'add_handlers':True},
+{'module':'__main__','log':'LOG','level':logging.DEBUG,'add_handlers':True},
+{'module':'StagYY','log':'LOG','level':logging.DEBUG,'add_handlers':True},
+{'module':'wallander','log':'LOG','level':logging.DEBUG,'add_handlers':True},
+{'module':'wallander.application','log':'LOG','level':logging.DEBUG,'add_handlers':False},
+{'module':'wallander.cache','log':'LOG','level':logging.DEBUG,'add_handlers':False},
+{'module':'wallander.viz','log':'LOG','level':logging.DEBUG,'add_handlers':False},
+{'module':'wallander.data_providers','log':'LOG','level':logging.DEBUG,'add_handlers':False},
+{'module':'wallander.contour','log':'LOG','level':logging.DEBUG,'add_handlers':False}
+]
 DEFAULT_LOG_LEVEL=logging.DEBUG
-# Add the log name and logging level, otherwise logging level will be DEFAULT_LOG_LEVEL
-# e.g.:  LOG_LEVELS={"wallander.LOG":logging.WARNING}
-LOG_LEVELS={}
 # The format of the log
 LOG_FORMATTER=logging.Formatter('%(asctime)s - %(name)s:%(lineno)d (%(process)d) - %(levelname)s - %(message)s')
 
@@ -54,48 +60,47 @@ def config_logging():
         configuration['log_handlers'].append(screen_handler)
     
     # The rotating file handler
-    logging_dir=configuration['log_dir']
-    if not os.path.exists(logging_dir):
-        os.makedirs(logging_dir)
-    print("config_logging: Logging directory %s"%logging_dir)
-    file_handler=None
-    file_number=0
-    while not file_handler and file_number<10:
-        try:
-            filename=os.path.join(logging_dir,'wallander%s.log'%('' if file_number<1 else '-%d'%file_number))
-            print("config_logging: Attempt logging to %s"%filename)
-            file_handler=TimedRotatingFileHandler(filename,when='midnight')
-            file_handler.setFormatter(LOG_FORMATTER)
-            file_handler.setLevel(logging.DEBUG)
-            configuration['log_handlers'].append(file_handler)
-            print("config_logging: Logging to %s"%filename)
-        except IOError:
-            file_number=file_number+1
+    if wallander.LOG_TO_FILE:
+        logging_dir=configuration['log_dir']
+        if not os.path.exists(logging_dir):
+            os.makedirs(logging_dir)
+        print("config_logging: Logging directory %s"%logging_dir)
+        file_handler=None
+        file_number=0
+        while not file_handler and file_number<10:
+            try:
+                filename=os.path.join(logging_dir,'wallander%s.log'%('' if file_number<1 else '-%d'%file_number))
+                print("config_logging: Attempt logging to %s"%filename)
+                file_handler=TimedRotatingFileHandler(filename,when='midnight')
+                file_handler.setFormatter(LOG_FORMATTER)
+                file_handler.setLevel(logging.DEBUG)
+                configuration['log_handlers'].append(file_handler)
+                print("config_logging: Logging to %s"%filename)
+            except IOError:
+                file_number=file_number+1
     print("config_logging: Log handlers: %s"%repr(configuration['log_handlers']))
    
 
-def add_logger(log_name,log_level=DEFAULT_LOG_LEVEL,handlers=None):
+def add_logger(log,handlers=None):
+    print(log)
+    print("Adding %(module)s.%(log)s at level %(level)s"%log)
     try:
-        if isinstance(log_name,str): 
-            ndxdot=log_name.rfind('.')
-            mod=log_name[:ndxdot]
-            log=log_name[ndxdot+1:]
-            mod=importlib.import_module(mod)
-            if log in mod.__dict__:
-                log=mod.__getattribute__(log)
-            else:
-                print("Couldn't find logger '%s' in '%s'",log,mod)
-                LOG.warning("Couldn't find logger '%s' in '%s'",log,mod)
-                return
+        mod=importlib.import_module(log['module'])
+        log_name=log['log']
+        if log_name in mod.__dict__:
+            logger=mod.__getattribute__(log_name)
         else:
-             log=log_name
-        print("add_logger (configuration): Log handlers: %s"%repr(configuration['log_handlers']))
-        handlers=handlers or configuration['log_handlers']
-        print("add_logger (handlers) Log handlers: %s"%repr(handlers))
-        map(log.addHandler,handlers or configuration['log_handlers'])
-        log.setLevel(log_level)
+            print("Couldn't find logger '%s' in '%s'",log_name,mod)
+            LOG.warning("Couldn't find logger '%s' in '%s'",log_name,mod)
+            return
+        if log['add_handlers'] :
+            print("add_logger (configuration): Log handlers: %s"%repr(configuration['log_handlers']))
+            print("add_logger (handlers) Log handlers: %s"%repr(handlers))
+            handlers=handlers or configuration['log_handlers']
+            map(logger.addHandler,handlers)
+        logger.setLevel(log['level'])
     except:
-        LOG.exception("Error configurintg log: %s"%log_name)
+        LOG.exception("Error configurintg log: %(module)s.%(log)s"%log)
 
 config_logging()
 
@@ -112,7 +117,4 @@ for data_provider in os.listdir(configuration['data_provider_dir']):
         LOG.exception('Unable to load data provider "%s"',dp_name)
 
 # setup configuration logging
-for log in LOGS:
-    log_level=LOG_LEVELS.get(log,DEFAULT_LOG_LEVEL)
-    print("Adding %s at level %s"%(log,log_level))
-    add_logger(log,log_level)
+map(add_logger,LOGS)
